4dfc9b7aa768b96385712b097f1d5373036788bf
Devendra <devendra@udgama.com>
2015-01-03 01:44:26 +0530

    subscribe v2

---
+++ node.js/examples/subv2.js
@@ -0,0 +1,63 @@
+/* ---------------------------------------------------------------------------
+
+    Init PubNub and Get your PubNub API Keys:
+    http://www.pubnub.com/account#api-keys
+
+--------------------------------------------------------------------------- */
+
+var pubnub = require("./../pubnub.js").init({
+    publish_key   : "demo",
+    ssl : true,
+    subscribe_key : "demo",
+    'v2'    : true
+});
+
+var pubnub1 = require("./../pubnub.js").init({
+    publish_key   : "demo",
+    ssl : true,
+    subscribe_key : "demo",
+    'v2'    : true
+});
+
+/* ---------------------------------------------------------------------------
+Listen for Messages
+--------------------------------------------------------------------------- */
+
+function subscribe_v2(channel) {
+    pubnub.subscribe({
+        v2       : true,
+        channel  : channel,
+        callback  : function(m,c,rc,e){
+            console.log('MESSAGE : ' + JSON.stringify(m,null,2));
+            console.log('CHANNEL : ' + JSON.stringify(c));
+            console.log('REAL CHANNEL : ' + JSON.stringify(rc));
+            console.log('COMPLETE MESSAGE : ' + JSON.stringify(e,null,2));
+        },
+        error : function(e) {
+            console.log(JSON.stringify(e));
+        },
+        connect : function(c) {
+            console.log('CONNECT : ' + JSON.stringify(c));
+            function publish(channel, msg) {
+                pubnub1.publish({
+                    channel  : channel,
+                    message  : msg,
+                    callback : log,
+                    error    : retry
+                });
+                }
+                function log(e) { console.log(e) }
+                function retry() { console.log('retry?')
+            }
+            publish('abcd','hi');
+        },
+        disconnect : function(c) {
+            console.log('DISCONNECT : ' + JSON.stringify(c));
+        },
+        reconnect : function(c) {
+            console.log('RECONNECT : ' + JSON.stringify(c));
+        }
+    });
+}
+
+subscribe_v2('abcd','hi');
